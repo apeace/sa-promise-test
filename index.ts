7885d9f95a370eb33b9f71cb8086d15b14c3f4bd
Andrew Peace <apeace@gmail.com>
2016-08-24 16:02:25 -0400

    Annotate scratchpad

--- index.ts
+++ index.ts
@@ -15,11 +15,10 @@ request
   .set('X-Awesome', 'superagent-promise')
   .end()
   .then(r => {
-    r.status;
-    console.log(`Status: ${r.status}`);
+    //console.log(`Status: ${r.status}`);
   })
   .catch(err => {
-    console.error(err);
+    //console.error(err);
   });
 
 /**
@@ -62,38 +61,69 @@ doThing(1, (v: number): Thing2<number> => new Thing2(v)).getThing();
  * Scratchpad, playing with passing a generic promise constructor
  */
 
+/**
+ * PromiseGiver represents what SuperAgentRequest should be.
+ * The givePromise() method represents what the end() method should be.
+ * It carries the type of the promise, P.
+ */
 interface PromiseGiver<V, P extends PromiseLike<V>> {
     givePromise(): P
 }
 
+/**
+ * An example of an alternate promise implementation, like BlueBird.
+ * (But only signatures implemented)
+ */
 class CustomPromise<V> {
     constructor(executor: (resolve: (value?: V | PromiseLike<V>) => void, reject: (reason?: any) => void) => void) {}
     then<TResult>(onfulfilled?: (value: V) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => TResult | PromiseLike<TResult>): PromiseLike<TResult> {
         return null;
     }
-    /*
-    then<TResult>(onfulfilled?: (value: V) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => void): PromiseLike<TResult> {
-        return null;
-    }
-    */
 }
 
+/**
+ * Manually creating a PromiseGiver with Promise
+ */
 let giver1: PromiseGiver<number, Promise<number>> = {
     givePromise(): Promise<number> {
         return new Promise((resolve, reject) => resolve(1));
     }
 };
+// prom1 is of type Promise<number>
 let prom1 = giver1.givePromise();
+prom1
+  .then(n => {
+    // type of n is number
+    console.log(n + 1 - 1);
+  });
 
+/**
+ * Manually creating a PromiseGiver with CustomPromise
+ */
 let giver2: PromiseGiver<number, CustomPromise<number>> = {
     givePromise(): CustomPromise<number> {
         return new CustomPromise((resolve, reject) => resolve(2));
     }
 };
+// prom2 is of type CustomPromise<number>
 let prom2 = giver2.givePromise();
+prom2.then(n => {
+    // type of n is number
+    // (but won't actually print because CustomPromise not really implemented)
+    console.log(n + 1 - 1);
+});
 
+/**
+ * A copy/paste/modify of PromiseConstructorLike. I did this because PromiseConstructorLike
+ * does not expost its generic type T.
+ */
 type PromCtorLike<T, P extends PromiseLike<T>> = new (executor: (resolve: (value?: T | P) => void, reject: (reason?: any) => void) => void) => P
 
+/**
+ * createPromiseGiver represents what request() should be in the superagent-promise typings.
+ * In this example, it takes a promise constructor and a value, and should return an instance
+ * of the given constructor with the given value.
+ */
 function createPromiseGiver<V, P extends PromiseLike<V>> (klass: PromCtorLike<V, P>, val: V): PromiseGiver<V, P> {
     return {
         givePromise(): P {
@@ -102,8 +132,35 @@ function createPromiseGiver<V, P extends PromiseLike<V>> (klass: PromCtorLike<V,
     };
 }
 
+/**
+ * Create a PromiseGiver with createPromiseGiver() and Promise.
+ * prom3 is of type Promise<number>
+ */
 let prom3 = createPromiseGiver<number, Promise<number>>(Promise, 3).givePromise();
-let prom4 = createPromiseGiver<number, CustomPromise<number>>(CustomPromise, 4).givePromise();
+prom3.then(n => {
+    // n is of type number
+    console.log(n + 1 - 1);
+});
 
-let prom5 = createPromiseGiver(Promise, 3).givePromise();
-let prom6 = createPromiseGiver(CustomPromise, 4).givePromise();
+/**
+ * Create a PromiseGiver with createPromiseGiver() and CustomPromise.
+ * prom4 is of type CustomPromise<number>
+ */
+let prom4 = createPromiseGiver<number, CustomPromise<number>>(CustomPromise, 4).givePromise();
+prom4.then(n => {
+    // type of n is number
+    // (but won't actually print because CustomPromise not really implemented)
+    console.log(n + 1 - 1);
+});
+
+/**
+ * Without the type parameters on createPromiseGiver(), we loose the type information.
+ * prom5 is of type PromiseLike<any>
+ */
+let prom5 = createPromiseGiver(Promise, 5).givePromise();
+prom5.then(n => {
+    // type of n is any
+    // therefore this compiles even though it shouldn't
+    // (will print undefined)
+    console.log(n.length);
+});
